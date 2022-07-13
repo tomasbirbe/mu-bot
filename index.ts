8a5c774ae6c05eb88c22ef1b54b27e2a9deb7100
Tomas <tomas.birbe@gmail.com>
2022-07-12 21:59:19 -0300

    testing-render: Mas logs para ver por que no funciona en render

--- index.ts
+++ index.ts
@@ -6,6 +6,11 @@ import { checkLvl } from './utils/character';
 
 config();
 
+interface Character {
+  name: string;
+  lvl: number;
+  reset: number;
+}
 const { TOKEN, SERVER_URL } = process.env;
 
 if (TOKEN && SERVER_URL) {
@@ -13,36 +18,60 @@ if (TOKEN && SERVER_URL) {
 
   app.use(json());
 
-  app.post('/', async (req, res) => {
+  app.post('/', (req, res) => {
     const { message } = req.body;
 
-    const character = await checkLvl(message.text.trim().toLocaleLowerCase());
+    checkLvl(message.text.trim().toLocaleLowerCase())
+      .then((character: Character) => {
+        const text = `A ${message.text.trim()} le faltan ${400 - character['lvl']} para resetear`;
 
-    if (character) {
-      const text = `A ${message.text.trim()} le faltan ${400 - character['lvl']} para resetear`;
+        axios({
+          method: 'POST',
+          url: `https://api.telegram.org/bot${TOKEN}/sendMessage`,
+          data: {
+            chat_id: message.chat.id,
+            text,
+          },
+        })
+          .then(() => res.send().status(200))
+          .catch((e) => console.log(e));
+      })
+      .catch((e) =>
+        axios({
+          method: 'POST',
+          url: `https://api.telegram.org/bot${TOKEN}/sendMessage`,
+          data: {
+            chat_id: message.chat.id,
+            text: 'Ese personaje no se encuentra en el ranking',
+          },
+        }).then(() => res.send().status(404)),
+      );
 
-      axios({
-        method: 'POST',
-        url: `https://api.telegram.org/bot${TOKEN}/sendMessage`,
-        data: {
-          chat_id: message.chat.id,
-          text,
-        },
-      })
-        .then(() => res.send().status(200))
-        .catch((e) => console.log(e));
-    } else {
-      axios({
-        method: 'POST',
-        url: `https://api.telegram.org/bot${TOKEN}/sendMessage`,
-        data: {
-          chat_id: message.chat.id,
-          text: 'Ese personaje no se encuentra en el ranking',
-        },
-      })
-        .then(() => res.send().status(404))
-        .catch((e) => console.log(e));
-    }
+    //   if (character) {
+    //     const text = `A ${message.text.trim()} le faltan ${400 - character['lvl']} para resetear`;
+
+    //     axios({
+    //       method: 'POST',
+    //       url: `https://api.telegram.org/bot${TOKEN}/sendMessage`,
+    //       data: {
+    //         chat_id: message.chat.id,
+    //         text,
+    //       },
+    //     })
+    //       .then(() => res.send().status(200))
+    //       .catch((e) => console.log(e));
+    //   } else {
+    //     axios({
+    //       method: 'POST',
+    //       url: `https://api.telegram.org/bot${TOKEN}/sendMessage`,
+    //       data: {
+    //         chat_id: message.chat.id,
+    //         text: 'Ese personaje no se encuentra en el ranking',
+    //       },
+    //     })
+    //       .then(() => res.send().status(404))
+    //       .catch((e) => console.log(e));
+    //   }
   });
 
   app.listen(3000, () => {
--- utils/character.ts
+++ utils/character.ts
@@ -7,42 +7,42 @@ interface Character {
   reset: number;
 }
 
-export async function checkLvl(name: string) {
+export function checkLvl(name: string): Promise<Character> {
   const { SERVER_URL } = process.env;
 
-  try {
-    const { data: rankingPage } = await axios(SERVER_URL || 'https://mu.cafe/rankings/resets');
+  return new Promise((resolve, reject) => {
+    return axios('https://mu.cafe/rankings/resets/').then(({ data: rankingPage }) => {
+      const $ = load(rankingPage);
 
-    const $ = load(rankingPage);
+      const rankingTable = $('.rankings-table > tbody');
+      const rankingRows = $(rankingTable).children('tr');
 
-    const rankingTable = $('.rankings-table > tbody');
-    const rankingRows = $(rankingTable).children('tr');
+      let character: Character | null = null;
 
-    let character: Character | null = null;
+      rankingRows.each((idx, row) => {
+        const characterDataElement = $(row).children('td');
+        // Each td is an array like:
+        // [position, img, classImg, name, activeIcon, lvl, reset, map]
+        const characterData: Character = {
+          name: $(characterDataElement.contents().get(3)).text().trim().toLocaleLowerCase(),
+          lvl: Number($(characterDataElement.contents().get(5)).text()),
+          reset: Number($(characterDataElement.contents().get(6)).text()),
+        };
 
-    rankingRows.each((idx, row) => {
-      const characterDataElement = $(row).children('td');
-      // Each td is an array like:
-      // [position, img, classImg, name, activeIcon, lvl, reset, map]
-      const characterData: Character = {
-        name: $(characterDataElement.contents().get(3)).text().trim().toLocaleLowerCase(),
-        lvl: Number($(characterDataElement.contents().get(5)).text()),
-        reset: Number($(characterDataElement.contents().get(6)).text()),
-      };
+        console.log(characterData.name, name.trim().toLocaleLowerCase());
 
-      console.log(characterData.name, name.trim().toLocaleLowerCase());
+        if (characterData.name === name.trim().toLocaleLowerCase()) {
+          character = characterData;
 
-      if (characterData.name === name.trim().toLocaleLowerCase()) {
-        character = characterData;
+          return false;
+        }
+      });
 
-        return false;
+      if (character) {
+        resolve(character);
+      } else {
+        reject(null);
       }
     });
-
-    if (character !== null) {
-      return character;
-    }
-  } catch (e) {
-    console.log(e);
-  }
+  });
 }
