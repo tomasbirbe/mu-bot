c0e7ad405d8e3cdcc3c2d7165d1c4adb397155f8
Tomas <tomas.birbe@gmail.com>
2022-07-13 19:26:51 -0300

    testing-render: modificaciones para que solucionar problemas de render

--- utils/getCharacterData.ts
+++ utils/getCharacterData.ts
@@ -21,7 +21,7 @@ export function getCharacterData(characterName: string): Promise<Character> {
         const $ = load(characterPage);
         const characterNameFromPage = $('.cname ').text();
 
-        console.log('Cheerio load the page');
+        console.log('Cheerio load the page', characterNameFromPage);
         if (characterNameFromPage.trim().toLocaleLowerCase() === characterNameNormalized) {
           const characterDataRows = $(
             '.profiles_player_table.profiles_player_table_info > tbody > tr',
