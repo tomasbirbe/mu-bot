b66347b044f164e03c811902d2c0db7c1351471a
Tomas <tomas.birbe@gmail.com>
2022-07-13 23:01:59 -0300

    testing-render: Mas logs para ver por que no funciona en render

--- index.ts
+++ index.ts
@@ -44,59 +44,7 @@ if (TOKEN && SERVER_URL) {
       });
   });
 
-  // app.post('/', (req, res) => {
-  // const { message } = req.body;
-  // checkLvl(message.text.trim().toLocaleLowerCase());
-  // .then((character: Character) => {
-  //   const text = `A ${message.text.trim()} le faltan ${400 - character['lvl']} para resetear`;
-  //   axios({
-  //     method: 'POST',
-  //     url: `https://api.telegram.org/bot${TOKEN}/sendMessage`,
-  //     data: {
-  //       chat_id: message.chat.id,
-  //       text,
-  //     },
-  //   })
-  //     .then(() => res.send().status(200))
-  //     .catch((e) => console.log(e));
-  // })
-  // .catch((e: any) =>
-  //   axios({
-  //     method: 'POST',
-  //     url: `https://api.telegram.org/bot${TOKEN}/sendMessage`,
-  //     data: {
-  //       chat_id: message.chat.id,
-  //       text: 'Ese personaje no se encuentra en el ranking',
-  //     },
-  //   }).then(() => res.send().status(404)),
-  // );
-  //   if (character) {
-  //     const text = `A ${message.text.trim()} le faltan ${400 - character['lvl']} para resetear`;
-  //     axios({
-  //       method: 'POST',
-  //       url: `https://api.telegram.org/bot${TOKEN}/sendMessage`,
-  //       data: {
-  //         chat_id: message.chat.id,
-  //         text,
-  //       },
-  //     })
-  //       .then(() => res.send().status(200))
-  //       .catch((e) => console.log(e));
-  //   } else {
-  //     axios({
-  //       method: 'POST',
-  //       url: `https://api.telegram.org/bot${TOKEN}/sendMessage`,
-  //       data: {
-  //         chat_id: message.chat.id,
-  //         text: 'Ese personaje no se encuentra en el ranking',
-  //       },
-  //     })
-  //       .then(() => res.send().status(404))
-  //       .catch((e) => console.log(e));
-  //   }
-  // });
-
   app.listen(PORT || 3000, () => {
-    console.log('App running on port 3000');
+    console.log(`App running on port ${PORT}`);
   });
 }
